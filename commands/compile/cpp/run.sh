f36326424cface593ccff237606f7ed34f06780e
vboxuser <vboxuser@Bot.myguest.virtualbox.org>
2023-07-27 00:40:51 -0400

    debugged and added input support for python, more debugging and formatting

--- commands/compile/cpp/run.sh
+++ commands/compile/cpp/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo g++ ./commands/compile/cpp/code.cpp -o ./commands/compile/cpp/script
-sudo ./commands/compile/cpp/script < ./commands/compile/cpp/in.txt > ./commands/compile/out.txt
+g++ ./commands/compile/cpp/code.cpp -o ./commands/compile/cpp/script
+./commands/compile/cpp/script < ./commands/compile/cpp/in.txt > ./commands/compile/out.txt
--- commands/compile/python/run.sh
+++ commands/compile/python/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python3 ./commands/compile/python/code.py > ./commands/compile/out.txt
+
+python3 ./commands/compile/python/code.py < ./commands/compile/python/in.txt > ./commands/compile/out.txt
